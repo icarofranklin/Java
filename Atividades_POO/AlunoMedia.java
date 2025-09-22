cbc0889c8e2062ef528f8eb20ab4fa51331e1341
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2025-09-22 10:26:33 -0300

    Atividades Faculdade
    Algoritmo para calcular a média dos alunos
    - O programa recebe dois valores de nota e calcula a média
    
    Algoritmo de conversão de tempo:
    O programa calcula e exibe duas saídas:
    - O tempo total convertido para minutos.
    - O total de minutos convertido para segundos.

---
+++ Atividades_POO/AlunoMedia.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+//importa a classe Scanner para leitura de dados
+
+public class AlunoMedia {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in); 
+        //1 - Cria um objeto Scanner para ler dados do teclado
+
+        System.out.print("Digite a primeiro nota: ");
+        //2 - Solicita ao usuário que digite a primeira nota
+        double nota1 = leitor.nextDouble(); 
+        //3 - Lê a primeira nota como um número de ponto flutuante
+
+        System.out.print("Digite a segunda nota: ");
+        //2 - Solicita ao usuário que digite a segunda nota
+        double nota2 = leitor.nextDouble(); 
+        //4 - Lê a segunda nota como um número de ponto flutuante
+
+        double media = (nota1 + nota2) / 2; 
+        //5 - Calcula a média das duas notas
+
+        System.out.println("A média do aluno é: " + media);
+        //6 - Exibe a média calculada
+
+        leitor.close(); 
+        //7 - Fecha o leitor para liberar recursos(boa prática)
+    }
+}
+// o que é ponto flutuante? um ponto flutuante (ou "float") é um tipo de dado usado para representar números reais na computação, permitindo valores com casas decimais e uma grande variedade de magnitudes, como 2,5 ou -3,14159.
---
+++ Atividades_POO/ConversorHora.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//importa a classe Scanner para leitura de dados
+public class ConversorHora {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        // Cria o leitor de teclado
+
+        System.out.print("Digite a hora (apenas números): ");
+        int hora = leitor.nextInt();
+        // Lê a hora
+        System.out.print("Digite os minutos (apenas números): ");
+        int minutos = leitor.nextInt();
+        // Lê os minutos
+        int totalMinutos = (hora * 60) + minutos;
+        // Converte tudo para minutos
+        int totalSegundos = totalMinutos * 60;
+        // Converte tudo para segundos
+        System.out.println("\nResultados:"); 
+        // Exibe a string resultados + quebra de linha
+        System.out.println("O tempo digitado equivale a: " + totalMinutos + " minutos."); 
+        // Exibe o total em minutos 
+        System.out.println("O tempo digitado equivale a: " + totalSegundos + " segundos.");
+        // Exibe o total em segundos
+        leitor.close();
+        // Fecha o leitor para liberar memória (boa prática)
+    }
+}
