78afb2bbc9bb5d3f6df4b0c3572c700aadebfd59
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2025-09-28 11:51:06 -0300

    Código simples
    Verificação par ou impar

--- Atividades_POO/Slide_02/ParOuImpar.java
+++ Atividades_POO/Slide_02/ParOuImpar.java
@@ -1,5 +1,14 @@
-
+import java.util.Scanner;
 public class ParOuImpar {
-    
-    
+    public static void main(String[] args) {
+    System.out.println("Digite um número inteiro:");
+        try (Scanner scanner = new Scanner(System.in)) { // Cria um objeto Scanner para ler a entrada do usuário
+        // fecha automaticamente o scanner
+            int numero = scanner.nextInt();
+            if (numero % 2 == 0) { // O número é dividido por 2 e o resto tem que ser igual a 0
+                System.out.println("O número " + numero + " é par.");
+            } else {
+                System.out.println("O número " + numero + " é ímpar.");
+            }   }
+    }
 }
