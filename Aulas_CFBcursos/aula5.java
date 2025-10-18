05dd732c5492e69d3ce94ec2245655c13f82a7b0
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2025-10-18 20:52:36 -0300

    Part1

--- Aulas_CFBcursos/aula5.java
+++ Aulas_CFBcursos/aula5.java
@@ -1,3 +1,11 @@
 public class aula5 {
-    
+    public static void main(String[] args) {
+
+        //FOR
+        for(int cont=5; cont > 0; cont--){ 
+        //cont++ é o mesmo que cont = cont + 1 ou cont += 1
+            System.out.println("Ícaro Franklin");
+        }
+        System.out.println("Fim do Programa");
+    }
 }
