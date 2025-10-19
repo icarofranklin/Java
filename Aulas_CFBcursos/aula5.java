ba24eb81241ef488da55b613dd3aae0ae7f5e18b
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2025-10-18 21:04:00 -0300

    Part2
    While

--- Aulas_CFBcursos/aula5.java
+++ Aulas_CFBcursos/aula5.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
 public class aula5 {
     public static void main(String[] args) {
 
-        //FOR
-        for(int cont=5; cont > 0; cont--){ 
+        Scanner scan = new Scanner(System.in);
+
+        /*FOR
+        for(int cont=0; cont < 12; cont++){ 
         //cont++ é o mesmo que cont = cont + 1 ou cont += 1
-            System.out.println("Ícaro Franklin");
-        }
+        System.out.println(cont + " - Ícaro Franklin");*/
+
+        //WHILE - usado quando não sabemos o número de repetições
+            System.out.println("Digite um número máximo: ");
+            int max = scan.nextInt();
+            int cont = 0; //inicialização
+            while(cont < max){//condição
+                System.out.println(cont + " - Ícaro Franklin");
+                cont++; //incremento
+            }
         System.out.println("Fim do Programa");
     }
 }
