25f5bb8cb5e234217312fbd2e729907f90797b44
Icaro <icarofranklins@gmail.com>
2026-02-13 09:21:59 -0300

    Revisão dos assuntos da aula 01 até 05

--- Aulas_CFBcursos/SwitchTeste.java
+++ Aulas_CFBcursos/SwitchTeste.java
@@ -3,14 +3,14 @@ public class SwitchTeste {
         // Estrutura Switch em Java
         // continuação da aula3.java
 
-        int pos=1; // posição no pódio
+        int pos=7; // posição no pódio
 
         switch(pos){
             case 1 -> System.out.println("Primeiro lugar, o mior, siiiiiiir"); 
             case 2 -> System.out.println("Segundo lugar");
             case 3 -> System.out.println("Terceiro lugar");
             case 4, 5, 6 -> System.out.println("Premiação de participação, para não voltar de mãos vazias");
-            default -> System.out.println("Não está no pódio\nPress F to pay respects");
+            default -> System.out.println("Não sobrou nada\nPress F to pay respects");
             // break não é necessário com a sintaxe "->"
         }
     }
--- Aulas_CFBcursos/aula5.java
+++ Aulas_CFBcursos/aula5.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
+
 public class aula5 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        /*FOR
-            for(int cont=0; cont < 12; cont++){ 
-            //cont++ é o mesmo que cont = cont + 1 ou cont += 1
-            System.out.println(cont + " - Ícaro Franklin");*/
+        // Estruturas de Repetição - FOR, WHILE e DO WHILE
 
-        /*WHILE - usado quando não sabemos o número de repetições
+        /*FOR é como se fosse uma parada de ônibus, onde você tem um ponto de partida, um destino e um número de paradas.
+            for(int cont=0; cont < 12; cont++){ 
+            System.out.println(cont + " - Ícaro Franklin");
+            }
+            cont++ é o mesmo que: 
+            - cont + 1 
+            - cont += 1
+            É bom executar ele quando você sabe a quantidade de repetições que o bloco de código deve ser executado.
+        */
+
+        /*WHILE - Usado quando não sabemos o número de repetições exatas, mas sabemos a condição de parada.
             System.out.println("Digite um número máximo: ");
             int max = scan.nextInt();
             int cont = 0; //inicialização
-            while(cont < max){//condição
+            while(cont < max){ //condição
                 System.out.println(cont + " - Ícaro Franklin");
-                cont++; //incremento */
+                cont++; //incremento 
+        */
                 
         //DO WHILE - usado quando não sabemos o número de repetições, mas queremos que o bloco seja executado pelo menos uma vez
             int cont = 0; //inicialização
             do { 
                 System.out.println(cont + " - Ícaro Franklin");
                 cont++; //incremento
-            } while (cont < 0); //condição
+            } while (cont < 6); //condição
         System.out.println("Fim do Programa");
     }
 }
