d6ce4dfddebb8a545090a74efc7a500211bae631
ICARO FERREIRA FRANKLIN <icarofranklins@gmail.com>
2026-08-25 05:13:33 -0300

    Add Spring Boot application structure with Maven wrapper and configuration files

---
+++ SpringBoot/backend/src/test/java/com/nutriexpress/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriexpress.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
